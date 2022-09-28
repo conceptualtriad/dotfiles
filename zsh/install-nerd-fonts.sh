cf2dfb5907a99f0d94e945184f7ed213c0d439b4
ConceptualTriad <53021957+conceptualtriad@users.noreply.github.com>
2022-09-28 18:18:13 -0400

    Romoves some fonts

--- zsh/install-nerd-fonts.sh
+++ zsh/install-nerd-fonts.sh
@@ -2,21 +2,12 @@
 
 declare -a fonts=(
     BitstreamVeraSansMono
-    CodeNewRoman
     DroidSansMono
     FiraCode
     FiraMono
-    Go-Mono
     Hack
-    Hermit
-    JetBrainsMono
     Meslo
-    Noto
-    Overpass
-    ProggyClean
     RobotoMono
-    SourceCodePro
-    SpaceMono
     Ubuntu
     UbuntuMono
 )
